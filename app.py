8770ed623eb79c1b45674b2db0fecdf177369e24
shishi8426 <shishi8426@gmail.com>
2025-06-16 22:45:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -89,3 +89,9 @@ def logout():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True, host='0.0.0.0', port=8000)
+
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+    
